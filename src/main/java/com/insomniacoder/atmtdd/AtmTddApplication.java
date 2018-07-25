26b3bec9dc113debd030cf82d8560c6247ac0bb1
InsomniaCoder <tanatloke@gmail.com>
2018-07-25 23:54:12 +0700

    init project

---
+++ src/main/java/com/insomniacoder/atmtdd/AtmTddApplication.java
@@ -0,0 +1,12 @@
+package com.insomniacoder.atmtdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmTddApplication.class, args);
+	}
+}
